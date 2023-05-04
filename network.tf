c97f6c451fc6002e19c36e36c467ca7f8c55b9f4
Jaden Lemmon <jaden@jadenlemmon.com>
2023-05-03 21:15:07 -0500

    adding resources

---
+++ network.tf
@@ -0,0 +1,31 @@
+resource "google_compute_network" "default" {
+  name                    = var.network_name
+  auto_create_subnetworks = "false"
+  project                 = var.project_id
+  routing_mode            = "REGIONAL"
+}
+
+resource "google_compute_address" "static-ingress" {
+  name     = "static-ingress"
+  project  = var.project_id
+  region   = var.region
+  provider = google-beta
+
+  labels = {
+    kubeip = "static-ingress"
+  }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "public-ingress"
+  network = google_compute_network.default.self_link
+  project = var.project_id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = var.node_pools_tags.ingress
+}
---
+++ pools.tf
@@ -0,0 +1,53 @@
+resource "google_container_node_pool" "node-pools" {
+  for_each = var.node_pools
+
+  depends_on         = [google_container_cluster.default]
+  name               = each.key
+  project            = var.project_id
+  location           = var.zone
+  cluster            = var.gke_cluster_name
+  initial_node_count = each.value.initial_node_count
+
+  autoscaling {
+    min_node_count = lookup(each.value, "min_node_count", 1)
+    max_node_count = lookup(each.value, "max_node_count", 2)
+  }
+
+  management {
+    auto_repair  = lookup(each.value, "auto_repair", true)
+    auto_upgrade = lookup(each.value, "auto_upgrade", true)
+  }
+
+  node_config {
+    machine_type    = lookup(each.value, "machine_type", "f1-micro")
+    disk_size_gb    = lookup(each.value, "disk_size_gb", 10)
+    disk_type       = lookup(each.value, "disk_type", "pd-standard")
+    spot            = lookup(each.value, "spot", false)
+    service_account = lookup(each.value, "service_account", "")
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/trace.append",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/servicecontrol",
+    ]
+
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
+
+    tags = var.node_pools_tags[each.key]
+
+    dynamic "taint" {
+      for_each = {
+        for obj in var.node_pools_taints[each.key] : "${obj.key}_${obj.value}_${obj.effect}" => obj
+      }
+      content {
+        effect = taint.value.effect
+        key    = taint.value.key
+        value  = taint.value.value
+      }
+    }
+  }
+}
